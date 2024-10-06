1be01f2b4e5254105f8a0285d38539b87ddd9e9a
Rafael Savvides <savvidesrafael@gmail.com>
2024-10-06 18:01:25 +0300

    Refactor data preparation
    Read midi file directly to fixed-step time series representation.

--- prepare_data.py
+++ prepare_data.py
@@ -8,17 +8,21 @@ START = "S"
 END = "E"
 
 
-def read_midi_to_events(file: Path | str, rest=REST) -> list[int | tuple | str, float]:
-    """Get note list from midi file
+def read_midi_to_time_series(
+    file: Path | str, rest=REST, hold=HOLD, step=0.25
+) -> list[int | tuple | str, float]:
+    """Read midi file to a fixed-step time series representation
+
+    Example: [52, 32, hold, hold, rest, 22, hold, ...] where all events are equidistant in time.
 
     Args:
         file: path to midi file
+        rest: symbol for a rest
+        hold: symbol for holding the previous note
+        step: Step size for time series (sampling step). Fraction of quarter note. Durations smaller than step are quantized to step. Defaults to 0.25.
 
     Returns:
-        list of (note, duration) tuples where:
-
-        - note: midi note number, tuple of midi note numbers, or rest.
-        - duration: fraction of quarter note
+        list of tokens (midi note numbers, rests, or holds)
     """
     song = m21.converter.parse(file)
     instruments = m21.instrument.partitionByInstrument(song).parts
@@ -34,27 +38,11 @@ def read_midi_to_events(file: Path | str, rest=REST) -> list[int | tuple | str, 
             note = event.notes[0].pitch.midi  # Save first note in chord.
         else:
             continue
-        notes.append((note, event.duration.quarterLength))
-    return notes
-
-
-def encode_song(note_list: list[int | str], step=0.25) -> str:
-    """Encode a list of (note, duration) events to a time series representation
-
-    Args:
-        note_list: list of (note, duration) events
-        step: Step size for time series (sampling step). Fraction of quarter note. Defaults to 0.25.
-
-    Returns:
-        string of events as a time series
-    """
-    encoded = [START]
-    for note, duration in note_list:
+        duration = event.duration.quarterLength
         num_steps = max(1, int(duration / step))
-        encoded.append(note)
-        encoded.extend([HOLD] * (num_steps - 1))
-    encoded.append(END)
-    return " ".join(str(n) for n in encoded)
+        notes.append(note)
+        notes.extend([hold] * (num_steps - 1))
+    return notes
 
 
 if __name__ == "__main__":
@@ -65,7 +53,6 @@ if __name__ == "__main__":
     path_to_processed.mkdir(parents=True, exist_ok=True)
 
     for file in tqdm(list(path_to_raw.glob("**/*.midi"))):
-        event_list = read_midi_to_events(file)
-        encoded_song = encode_song(event_list)
+        sequence = read_midi_to_time_series(file)
         with open(path_to_processed / (file.name + ".txt"), "w") as f:
-            f.write(encoded_song)
+            f.write(" ".join(str(e) for e in sequence))
