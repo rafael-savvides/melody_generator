47ecd555e2d84dfa4cb19f1633b0f298a7087e64
Rafael Savvides <savvidesrafael@gmail.com>
2024-10-14 14:31:03 +0300

    Move pitch scaling as a transform in the data loader

--- train_model.py
+++ train_model.py
@@ -4,6 +4,7 @@ from torch.utils.data import DataLoader
 from pathlib import Path
 from torch import optim
 from prepare_data import read_event_sequence
+import numpy as np
 
 config = {
     "input_size": 3,
@@ -36,9 +37,10 @@ class MelodyLSTM(nn.Module):
         Returns:
             torch tensor of shape (seq_len, output_size)
         """
+        seq_len = len(x)
         # lstm's default h0 (hidden state) and c0 (cell state) are zeroes.
         lstm_out, _ = self.lstm(x)
-        out = self.fc(lstm_out.view(len(x), -1))
+        out = self.fc(lstm_out.view(seq_len, -1))
         # TODO One-hot encode pitches. Or use nn.Embedding?
         # TODO Enforce positive pitch, duration and offset (e.g. exp or softplus?)
         return out
@@ -59,8 +61,8 @@ def train_model(
 
             model.zero_grad()
 
-            inputs = scale_pitch(torch.tensor(inputs, dtype=torch.float32))
-            target = scale_pitch(torch.tensor(target, dtype=torch.float32))
+            inputs = torch.tensor(inputs, dtype=torch.float32)
+            target = torch.tensor(target, dtype=torch.float32)
 
             output = model(inputs)
             loss = loss_fn(output[-1], target)
@@ -70,30 +72,36 @@ def train_model(
     return model
 
 
-def scale_pitch(x, pitch_range=128):
+def scale_pitch(x: list | np.ndarray, pitch_range=128) -> np.ndarray:
     """Scale pitch
 
     Args:
-        x: Tuple of (pitch, duration, offset)
+        x: List of (pitch, duration, offset) tuples.
         pitch_range: Maximum pitch. Defaults to 128.
 
     Returns:
-        x with its first element divided by pitch_range
+        x with its `pitch` elements divided by `pitch_range`.
+        x is cast into a 2D numpy array.
+        If x is a single (pitch, duration, offset) tuple, it becomes a (1, 3) array.
     """
-    return x / torch.tensor([pitch_range, 1.0, 1.0])
+    return np.atleast_2d(x) / np.array([pitch_range, 1.0, 1.0])
 
 
 class EventSequenceDataset(torch.utils.data.Dataset):
-    def __init__(self, path: str | Path, sequence_length: int):
+    def __init__(
+        self, path: str | Path, sequence_length: int, transform: callable = None
+    ):
         """Event sequence dataset
 
         Args:
             path: Directory of files with comma-delimited tuples of midi pitch,duration,offset e.g. 36,0.25,0. Each line is an event.
             sequence_length: Sequence length to use as input for predicting the next item in the sequence.
+            transform: A function applied to both sequence and next_item.
         """
         self.path = path
         self.files = Path(path).glob("*.txt")
         self.sequence_length = sequence_length
+        self.transform = transform
         self.data = []
         for file in self.files:
             self.data.extend(read_event_sequence(file))
@@ -112,6 +120,8 @@ class EventSequenceDataset(torch.utils.data.Dataset):
         """
         sequence = self.data[idx : idx + self.sequence_length]
         next_item = self.data[idx + self.sequence_length]
+        if self.transform is not None:
+            sequence, next_item = self.transform(sequence), self.transform(next_item)
         return sequence, next_item
 
     def __len__(self):
@@ -126,7 +136,9 @@ if __name__ == "__main__":
     dataset = "event_sequence"  # 3696777 notes in 1276 files
     path_to_dataset_txt = Path(f"data/{dataset}")
     data = EventSequenceDataset(
-        path=path_to_dataset_txt, sequence_length=config["sequence_length"]
+        path=path_to_dataset_txt,
+        sequence_length=config["sequence_length"],
+        transform=scale_pitch,
     )
     data_loader = DataLoader(data, shuffle=True)
 
